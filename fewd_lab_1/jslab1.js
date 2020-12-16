4943d7aa3f9a758dde7ae8e85193edebd61f1b31
21565-Alessandra <71334857+21565-Alessandra@users.noreply.github.com>
2020-12-16 15:38:17 +0000

    Add files via upload

---
+++ fewd_lab_1/jslab1.js
@@ -0,0 +1,46 @@
+var firstName = 'Alessandra';
+var favouriteColor = 'Red';
+var city = 'Lubumbashi', 
+city = 'Bray';
+var country = 'Congo';
+var favouriteFood = 'Lasagna';
+
+    
+//The console.log() will print the variables defined before in this JS program.
+console.log(firstName);
+console.log(favouriteColor); 
+console.log(favouriteFood + ' - ' + city);
+
+
+var phrase = ' A rainbow is formed by the sunlight. Shining through rain drops so small. The rain bends the light from the sunshine, and colors the  sky for us all.';
+var endingPhrase = 'is what we see. Rainbow colours, are made with light bending you see!';
+var one = ' Red';
+var two = 'orange';
+var three = 'yellow';
+var four = 'green';
+var five = 'blue';
+var six = 'indigo';
+var seven = 'violet';
+
+console.log(phrase + ' ' + one + ' ' + two + ' ' + three + ' ' + four + ' ' + five + ' ' + six + ' ' + seven + ' ' + endingPhrase)
+
+
+
+
+/*
+QUESTION 6
+
+let x = 'Tic';
+    x = 'Tac';
+    x = 'Toe';
+    console.log(x);
+
+QUESTION 7
+
+var x = 'Laurel';
+var y = 'Hardy';
+var z = y;
+y = x;
+x = z;
+
+*/
